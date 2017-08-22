ca2f3fd4a5fb2a28cd1b5df772d589126cc97f13
宋倩倩 <huangyuyuan@ecmoban.comshuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-08-22 16:47:21 +0800

    更新公众平台

---
+++ mobile_userbind.php
@@ -0,0 +1,185 @@
+<?php
+//
+//    ______         ______           __         __         ______
+//   /\  ___\       /\  ___\         /\_\       /\_\       /\  __ \
+//   \/\  __\       \/\ \____        \/\_\      \/\_\      \/\ \_\ \
+//    \/\_____\      \/\_____\     /\_\/\_\      \/\_\      \/\_\ \_\
+//     \/_____/       \/_____/     \/__\/_/       \/_/       \/_/ /_/
+//
+//   上海商创网络科技有限公司
+//
+//  ---------------------------------------------------------------------------------
+//
+//   一、协议的许可和权利
+//
+//    1. 您可以在完全遵守本协议的基础上，将本软件应用于商业用途；
+//    2. 您可以在协议规定的约束和限制范围内修改本产品源代码或界面风格以适应您的要求；
+//    3. 您拥有使用本产品中的全部内容资料、商品信息及其他信息的所有权，并独立承担与其内容相关的
+//       法律义务；
+//    4. 获得商业授权之后，您可以将本软件应用于商业用途，自授权时刻起，在技术支持期限内拥有通过
+//       指定的方式获得指定范围内的技术支持服务；
+//
+//   二、协议的约束和限制
+//
+//    1. 未获商业授权之前，禁止将本软件用于商业用途（包括但不限于企业法人经营的产品、经营性产品
+//       以及以盈利为目的或实现盈利产品）；
+//    2. 未获商业授权之前，禁止在本产品的整体或在任何部分基础上发展任何派生版本、修改版本或第三
+//       方版本用于重新开发；
+//    3. 如果您未能遵守本协议的条款，您的授权将被终止，所被许可的权利将被收回并承担相应法律责任；
+//
+//   三、有限担保和免责声明
+//
+//    1. 本软件及所附带的文件是作为不提供任何明确的或隐含的赔偿或担保的形式提供的；
+//    2. 用户出于自愿而使用本软件，您必须了解使用本软件的风险，在尚未获得商业授权之前，我们不承
+//       诺提供任何形式的技术支持、使用担保，也不承担任何因使用本软件而产生问题的相关责任；
+//    3. 上海商创网络科技有限公司不对使用本产品构建的商城中的内容信息承担责任，但在不侵犯用户隐
+//       私信息的前提下，保留以任何方式获取用户信息及商品信息的权利；
+//
+//   有关本产品最终用户授权协议、商业授权与技术服务的详细内容，均由上海商创网络科技有限公司独家
+//   提供。上海商创网络科技有限公司拥有在不事先通知的情况下，修改授权协议的权力，修改后的协议对
+//   改变之日起的新授权用户生效。电子文本形式的授权协议如同双方书面签署的协议一样，具有完全的和
+//   等同的法律效力。您一旦开始修改、安装或使用本产品，即被视为完全理解并接受本协议的各项条款，
+//   在享有上述条款授予的权力的同时，受到相关的约束和限制。协议许可范围以外的行为，将直接违反本
+//   授权协议并构成侵权，我们有权随时终止授权，责令停止损害，并保留追究相关责任的权力。
+//
+//  ---------------------------------------------------------------------------------
+//
+
+defined('IN_ECJIA') or exit('No permission resources.');
+
+
+class mobile_userbind extends ecjia_front {
+    
+    public function __construct() {
+        parent::__construct();
+        
+        RC_Loader::load_app_class('platform_account', 'platform', false);
+        RC_Loader::load_app_class('wechat_user', 'wechat', false);
+        
+        $this->assign('front_url', RC_App::apps_url('statics/front', __FILE__));
+        $this->assign('system_statics_url', RC_Uri::admin_url('statics'));
+        $this->assign('logo_pic', RC_App::apps_url('statics/front/images/logo_pic.png', __FILE__));
+      
+    }
+    
+    
+    public function init() {
+    	$openid = trim($_GET['openid']);
+    	$uuid   = trim($_GET['uuid']);
+    	$account = platform_account::make($uuid);
+    	$wechat_id = $account->getAccountID();
+    	$wechat_user = new wechat_user($wechat_id, $openid);
+    	
+    	//获取微信用户信息
+    	$data['wechat_image'] 	= $wechat_user->getImage();
+    	$data['wechat_nickname'] = $wechat_user->getNickname();
+    	$data['register_url'] = RC_Uri::url('wechat/mobile_userbind/register', array('uuid' => $uuid, 'openid' => $openid));
+        $data['bind_url']  = RC_Uri::url('wechat/mobile_userbind/bind_login', array('openid' => $openid));
+        $this->assign('data', $data);
+
+        $this->display(
+            RC_Package::package('app::wechat')->loadTemplate('front/bind.dwt', true)
+        );
+    }
+
+    //注册登录
+    public function register() {
+    	$openid = trim($_GET['openid']);
+    	$uuid   = trim($_GET['uuid']);
+    	
+    	$account = platform_account::make($uuid);
+    	$wechat_id = $account->getAccountID();
+    	$wechat_user = new wechat_user($wechat_id, $openid);
+    	$username  = $wechat_user->getNickname();
+    	$unionid = $wechat_user->getUnionid();
+    	
+    	//创建新的用户
+    	$password  = wechat_user::generate_password();
+    	$email     = wechat_user::generate_email();
+    	$sex       = $wechat_user->sex();
+    	$reg_time  = RC_Time::gmtime();
+    	$user = RC_Api::api('user', 'init_user');
+    	if ($user && $user->check_user($username)) {
+    		$username = $username . rc_random(4, 'abcdefghijklmnopqrstuvwxyz0123456789');
+    	}
+    	$user_info = RC_Api::api('user', 'add_user', array('username' => $username, 'password' => $password, 'email' => $email, 'sex'=>$sex, 'reg_time'=>$reg_time));
+    	if (is_ecjia_error($user_info)) {
+    		ecjia_front::$controller->showmessage($user_info->get_error_message(), ecjia::MSGTYPE_JSON | ecjia::MSGSTAT_ERROR);
+    	} else {
+    		$user_id = $user_info['user_id'];
+    		$query = RC_DB::table('connect_user')->where('open_id', $uuid)->where('connect_code', $sns_wechat)->count();
+    		if($query > 0){
+    			RC_DB::table('connect_user')->where(array('open_id' => $unionid, 'connect_code'=>'sns_wechat'))->update(array('user_id' => $user_id));
+    		}else{
+    			$data['connect_code'] = 'sns_wechat';
+    			$data['user_id']   = $user_id;
+    			$data['is_admin']  = 0;
+    			$data['open_id']   = $unionid;
+    			$data['create_at'] = $reg_time;
+    			RC_DB::table('connect_user')->insertGetId($data);
+    			return $this->redirect(RC_Uri::url('mobile/mobile_profile/init', array('user_id' => $user_id, 'openid' => $openid)));
+    		}
+    	}
+    }
+    //已有账号绑定
+    public function bind_login() {
+    	$openid = trim($_GET['openid']);
+    	$this->assign('openid', $openid);
+    	
+    	$this->display(
+    		RC_Package::package('app::wechat')->loadTemplate('front/bind_signin.dwt', true)
+    	);
+    }
+    
+    //已有账号绑定
+    public function bind_login_insert() {
+    	$openid = trim($_GET['openid']);
+    	$user_name = trim($_GET['username']);
+    	$password = trim($_GET['password']);
+    	
+    	if (empty($user_name)) {
+    		return ecjia_front::$controller->showmessage('用户名不能为空！', ecjia::MSGTYPE_JSON | ecjia::MSGSTAT_ERROR);
+    	}
+    	
+    	if (empty($password)) {
+    		return ecjia_front::$controller->showmessage('密码不能为空！', ecjia::MSGTYPE_JSON | ecjia::MSGSTAT_ERROR);
+    	}
+    	//登录逻辑处理
+    	$user_db = RC_Loader::load_app_model('users_model','user');
+    	
+    	//判断已绑定用户
+    	$row = RC_DB::table('users')->where('user_name', $user_name)->first();
+    	if ($row) {
+    		if (!empty($row['ec_salt'])) {
+    			if (!($row['user_name'] == $user_name && $row['password'] == md5(md5($password) . $row['ec_salt']))) {
+    				return ecjia_front::$controller->showmessage('您输入账号信息不正确，绑定失败！', ecjia::MSGTYPE_JSON | ecjia::MSGSTAT_ERROR);
+    			}
+    		} else {
+    			if (!($row['user_name'] == $user_name && $row['password'] == md5($password))) {
+    				return ecjia_front::$controller->showmessage('您输入账号信息不正确，绑定失败！', ecjia::MSGTYPE_JSON | ecjia::MSGSTAT_ERROR);
+    			}
+    		}
+    		
+    		$connect_user = new \Ecjia\App\Connect\ConnectUser('sns_wechant', $openid);
+    		$result = $connect_user->bindUser($row['user_id']);
+    		if ($result) {
+    			return ecjia_front::$controller->showmessage('恭喜您，关联成功', ecjia::MSGTYPE_JSON | ecjia::MSGSTAT_SUCCESS, array('url' => RC_Uri::url('wechat/mobile_profile/init')));
+    		} else {
+    			return ecjia_front::$controller->showmessage('抱歉，关联失败', ecjia::MSGTYPE_JSON | ecjia::MSGSTAT_ERROR);
+    		}
+
+    		$_SESSION['user_id']   = $row['user_id'];
+    		$_SESSION['user_name'] = $row['user_name'];
+    		$_SESSION['email']     = $row['email'];
+    		$_SESSION['last_ip']   = RC_Ip::client_ip();
+    		$_SESSION['last_time'] = RC_Time::gmtime();
+    		$data = array(
+    			'last_login' => RC_Time::gmtime(),
+    			'last_ip'    => RC_Ip::client_ip()
+    		);
+    		RC_DB::table('users')->where('user_id', $row['user_id'])->update($data);
+    	} else {
+    		return ecjia_front::$controller->showmessage('您输入账号信息不正确，绑定失败！', ecjia::MSGTYPE_JSON | ecjia::MSGSTAT_ERROR);
+    	}	
+    }
+}
---
+++ templates/front/bind_signin.dwt.php
@@ -0,0 +1,58 @@
+{nocache}
+<!DOCTYPE html>
+<html>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0"/>
+<meta http-equiv="X-UA-Compatible" content="IE=8,IE=9,IE=10,IE=11"/>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<head lang="zh-CN">
+		<title>绑定账号</title>
+		<link rel="stylesheet" type="text/css" href="{$front_url}/css/touch.css" />
+		<link rel="stylesheet" type="text/css" href="{$front_url}/css/style.css" />
+	</head>
+	
+	<header class="ecjia-header">
+		<div class="ecjia-header-left">
+		</div>
+		<div class="ecjia-header-title">关联账号</div>
+	</header>
+	<body>
+		<div class="ecjia-form  ecjia-login">
+			<div class="form-group margin-right-left ">
+				<label class="input">
+					<i class="iconfont icon-dengluyonghuming"></i>
+					<input placeholder="{$lang.name_or_mobile}" name="username" datatype="*3-16|zh2-7" errormsg="用户名错误请重新输入！" nullmsg="请输入用户名户或手机号" />
+				</label>
+			</div>
+			
+			<div class="form-group ecjia-margin-t margin-right-left">
+				<label class="input">
+					<i class="iconfont icon-lock "></i>
+					<i class="iconfont icon-attention ecjia-login-margin-l"  id="password1"></i>
+					<input placeholder="{$lang.input_passwd}" name="password" type="password" id="password-1" datatype="*6-16" errormsg="密码错误请重新输入！" nullmsg="请输入密码" />
+				</label>
+			</div>
+			
+			<div class="ecjia-login-b">
+			    <div class="around">
+			        <input type="hidden" name="openid" value="{$openid}">
+			        <a class="ecjia-bind-login btn" data-url="{url path='wechat/mobile_userbind/bind_login_insert'}">关联</a>
+			    </div>	
+			</div>
+		</div>
+		
+		<script src="{$system_statics_url}/js/jquery.min.js" type="text/javascript"></script>
+        <script src="{$system_statics_url}/lib/ecjia-js/ecjia.js" type="text/javascript"></script>
+        
+        <script src="{$front_url}/js/bind.js" type="text/javascript"></script>
+        
+        <script src="{$system_statics_url}/lib/chosen/chosen.jquery.min.js" type="text/javascript"></script>
+        <script src="{$system_statics_url}/js/jquery-migrate.min.js" type="text/javascript"></script>
+        <script src="{$system_statics_url}/lib/uniform/jquery.uniform.min.js" type="text/javascript"></script>
+        <script src="{$system_statics_url}/lib/smoke/smoke.min.js" type="text/javascript"></script>
+        <script src="{$system_statics_url}/js/jquery-cookie.min.js" type="text/javascript"></script>
+        <script type="text/javascript">
+       		 ecjia.bind.init();
+        </script>
+	</body>
+</html>
+{/nocache}
