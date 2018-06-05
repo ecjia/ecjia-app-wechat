7c3fb15fab48845c702e914712400191012099cf
吴体方 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comthuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comfhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-06-05 13:48:43 +0800

    修改扫码引荐列表

---
+++ templates/platform/wechat_share_edit.dwt.php
@@ -0,0 +1,100 @@
+<?php defined('IN_ECJIA') or exit('No permission resources.');?>
+<!-- {extends file="ecjia-platform.dwt.php"} -->
+
+<!-- {block name="footer"} -->
+<script type="text/javascript">
+	ecjia.platform.wechat_qrcodeshare_edit.init();
+</script>
+<!-- {/block} -->
+<!-- {block name="home-content"} -->
+
+<!-- {if $warn && $type neq 2} -->
+<div class="row">
+	<div class="col-12">
+	    <div class="alert alert-dismissible mb-2 alert-error">
+	    	<strong>{lang key='wechat::wechat.label_notice'}</strong>{$type_error}
+	    </div>
+	</div>
+</div>
+<!-- {/if} -->	
+
+<!-- {if $errormsg} -->
+<div class="row">
+	<div class="col-12">
+	    <div class="alert alert-dismissible mb-2 alert-error">
+	    	<strong>{lang key='wechat::wechat.label_notice'}</strong>{$errormsg}
+	    </div>
+	</div>
+</div>
+<!-- {/if} -->
+
+<div class="row">
+    <div class="col-12">
+        <div class="card">
+			<div class="card-header">
+                <h4 class="card-title">
+                	{$ur_here}
+	               	{if $action_link}
+					<a class="btn btn-light plus_or_reply data-pjax float-right" href="{$action_link.href}" id="sticky_a"><i class="fa fa-reply"></i> {$action_link.text}</a>
+					{/if}
+                </h4>
+            </div>
+            <div class="col-lg-12">
+				<form class="form" method="post" name="theForm" action="{$form_action}">
+					<div class="card-body">
+						<div class="form-body">
+							<div class="form-group row">
+								<label class="col-lg-2 label-control text-right">{lang key='wechat::wechat.label_recommended_person'}</label>
+								<div class="col-lg-8 controls">
+									<input type="text" name="username" id="username" class="form-control"/>
+									<span class="input-must">{lang key='system::system.require_field'}</span>
+								</div>
+							</div>
+							
+							<div class="form-group row">
+								<label class="col-lg-2 label-control text-right">{lang key='wechat::wechat.label_recommended_id'}</label>
+								<div class="col-lg-8 controls">
+									<input type="text" name="scene_id" id="scene_id" class="form-control"/>
+									<span class="input-must">{lang key='system::system.require_field'}</span>
+								</div>
+							</div>
+							
+							<div class="form-group row">
+								<label class="col-lg-2 label-control text-right">{lang key='wechat::wechat.label_expire_seconds'}</label>
+								<div class="col-lg-8 controls">
+									<input type="text" name="expire_seconds" id="expire_seconds" class="form-control"/>
+									<span class="help-block">{lang key='wechat::wechat.label_expire_seconds_help'}</span>
+								</div>
+							</div>
+							
+							<div class="form-group row">
+								<label class="col-lg-2 label-control text-right">{lang key='wechat::wechat.label_functions'}</label>
+								<div class="col-lg-8 controls">
+									<input type="text" name="functions" id="function" class="form-control"/>
+									<span class="input-must">{lang key='system::system.require_field'}</span>
+								</div>
+							</div>
+							
+							<div class="form-group row">
+								<label class="col-lg-2 label-control text-right">{lang key='wechat::wechat.label_sort'}</label>
+								<div class="col-lg-8 controls">
+									<input type="text" name="sort" id="sort" class="form-control"/>
+								</div>
+							</div>
+							
+						</div>
+					</div>
+	
+					<div class="modal-footer justify-content-center">
+						{if $errormsg || ($type_error && $type neq 2)}
+						<input type="submit" name="submit" value="{lang key='wechat::wechat.ok'}" class="btn btn-light" disabled="disabled" />	
+						{else}
+						<input type="submit" name="submit" value="{lang key='wechat::wechat.ok'}" class="btn btn-light" />	
+						{/if}
+					</div>
+				</form>	
+            </div>
+        </div>
+    </div>
+</div>
+<!-- {/block} -->
---
+++ templates/platform/wechat_share_list.dwt.php
@@ -0,0 +1,89 @@
+<?php defined('IN_ECJIA') or exit('No permission resources.');?>
+<!-- {extends file="ecjia-platform.dwt.php"} -->
+
+<!-- {block name="footer"} -->
+<script type="text/javascript">
+	ecjia.platform.wechat_qrcodeshare_list.init();
+</script>
+<!-- {/block} -->
+<!-- {block name="home-content"} -->
+
+<!-- {if $warn && $type neq 2} -->
+<div class="row">
+	<div class="col-12">
+	    <div class="alert alert-dismissible mb-2 alert-error">
+	    	<strong>{lang key='wechat::wechat.label_notice'}</strong>{$type_error}
+	    </div>
+	</div>
+</div>
+<!-- {/if} -->	
+
+<!-- {if $errormsg} -->
+<div class="row">
+	<div class="col-12">
+	    <div class="alert alert-dismissible mb-2 alert-error">
+	    	<strong>{lang key='wechat::wechat.label_notice'}</strong>{$errormsg}
+	    </div>
+	</div>
+</div>
+<!-- {/if} -->
+
+<div class="row">
+    <div class="col-12">
+        <div class="card">
+			<div class="card-header">
+                <h4 class="card-title">
+                	{$ur_here}
+	               	{if $action_link}
+					<a class="btn btn-light plus_or_reply data-pjax float-right" href="{$action_link.href}" id="sticky_a"><i class="ft-plus"></i> {$action_link.text}</a>
+					{/if}
+                </h4>
+            </div>
+            <div class="col-md-12">
+				<table class="table table-hide-edit">
+					<thead>
+						<tr>
+							<th class="w150">{lang key='wechat::wechat.recommended_person'}</th>
+							<th class="w100">{lang key='wechat::wechat.cash_into'}</th>
+							<th class="w100">{lang key='wechat::wechat.scan_num'}</th>
+							<th class="w150">{lang key='wechat::wechat.expire_seconds'}</th>
+							<th class="w200">{lang key='wechat::wechat.function'}</th>
+							<th class="w100">{lang key='wechat::wechat.sort'}</th>
+						</tr>
+					</thead>
+					<tbody>
+						<!-- {foreach from=$listdb.share_list item=val} -->
+						<tr>
+							<td class="hide-edit-area">
+								{$val.username}
+					    		<div class="edit-list">
+							     	{assign var=view_url value=RC_Uri::url('wechat/platform_qrcode/qrcode_get',"id={$val.id}")}
+						      		<a class="ajaxwechat" href="{$view_url}" title="{lang key='system::system.view'}">{lang key='wechat::wechat.get_qrcode'}</a>&nbsp;|&nbsp;
+						      		<a class="ajaxremove ecjiafc-red" data-toggle="ajaxremove" data-msg="{lang key='wechat::wechat.remove_qrcode_confirm'}" href='{RC_Uri::url("wechat/platform_share/remove","id={$val.id}")}' title="{lang key='system::system.drop'}">{lang key='system::system.drop'}</a>
+							    </div>
+							</td>
+							<td>
+								0
+							</td>
+							<td>
+								{$val['scan_num']}
+							</td>
+							<td>
+								{$val['expire_seconds']}
+							</td>
+							<td>
+								{$val.function}
+							</td>
+							<td><span class="cursor_pointer" data-trigger="editable" data-url="{RC_Uri::url('wechat/platform_share/edit_sort')}" data-name="sort" data-pk="{$val.id}"  data-title="{lang key='wechat::wechat.edit_qrcode_sort'}">{$val.sort}</span></td>
+						</tr>
+						<!--  {foreachelse} -->
+						<tr><td class="no-records" colspan="6">{lang key='system::system.no_records'}</td></tr>
+						<!-- {/foreach} -->
+					</tbody>
+				</table>						
+            </div>
+            <!-- {$listdb.page} -->
+        </div>
+    </div>
+</div>
+<!-- {/block} -->
