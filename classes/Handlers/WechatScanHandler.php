6ced402ebaee257a79bbd515c8942a994bb53375
东方皓 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-07-04 13:48:57 +0800

    增加微信公众号扫描事件处理器

--- classes/Handlers/WechatScanHandler.php
+++ classes/Handlers/WechatScanHandler.php
@@ -10,15 +10,39 @@ class WechatScanHandler
     
     protected $wechatUUID;
     
+    protected $eventType;
+    
+    protected $eventKey;
+    
+    protected $ticket;
+    
+    const EVENT_TYPE_SUBSCRIBE = 'subscribe';
+    
+    const EVENT_TYPE_SCAN = 'scan';
+    
     public function __construct($message)
     {
+        
         $this->message = $message;
         
         $this->wechatUUID = new WechatUUID();
+        
+        if ($this->message->get('Event') == 'subscribe') {
+            $this->eventType = self::EVENT_TYPE_SUBSCRIBE;
+            $this->eventKey = str_replace('qrscene_', '', $this->message->get('EventKey'));
+            
+        } else {
+            $this->eventType = self::EVENT_TYPE_SCAN;
+            $this->eventKey = $this->message->get('EventKey');
+        }
+        
+        $this->ticket = $this->message->get('Ticket');
     }
     
-    
-    public function scan()
+    /**
+     * 根据$this->eventKey参数决定做什么
+     */
+    public function getScanEventHandler()
     {
         
         
