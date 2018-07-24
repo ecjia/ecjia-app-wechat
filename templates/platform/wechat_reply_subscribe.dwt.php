e2ebc04d5359cbba12f0fabc86684f8b258204c8
吴体方 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comthuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comfhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-07-24 13:55:50 +0800

    修改自动回复选择素材

--- templates/platform/wechat_reply_subscribe.dwt.php
+++ templates/platform/wechat_reply_subscribe.dwt.php
@@ -4,6 +4,7 @@
 <!-- {block name="footer"} -->
 <script type="text/javascript">
 	ecjia.platform.response.init();
+	ecjia.platform.choose_material.init();
 </script>
 <!-- {/block} -->
 <!-- {block name="home-content"} -->
@@ -44,38 +45,75 @@
 					<div class="m_t10">
 						<div class="form-body">
 							<div class="form-group row">
-								<div class="col-lg-12 controls material-table" data-url="{url path='wechat/platform_response/get_material_list'}">
+								<div class="col-lg-12 controls material-table">
 									<ul class="nav nav-tabs nav-only-icon nav-top-border no-hover-bg">
-										<li class="nav-item text-material">
-											<a class="nav-link active" data-toggle="tab" title="{lang key='wechat::wechat.text'}"><i class="fa fa-pencil"> 文字</i></a>
+										<li class="nav-item" data-type="text">
+											<a class="nav-link {if $subscribe.reply_type eq 'text'}active{/if}" data-toggle="tab" title="{lang key='wechat::wechat.text'}"><i class="fa fa-pencil"> 文字</i></a>
 										</li>
-										<li class="nav-item picture-material">
-											<a class="nav-link" data-toggle="tab" title="{lang key='wechat::wechat.image'}"><i class="fa fa-file-image-o"> 图片</i></a>
+										<li class="nav-item" data-type="image">
+											<a class="nav-link {if $subscribe.reply_type eq 'image'}active{/if}" data-toggle="tab" title="{lang key='wechat::wechat.image'}"><i class="fa fa-file-image-o"> 图片</i></a>
 										</li>
-										<li class="nav-item music-material">
-											<a class="nav-link" data-toggle="tab" title="{lang key='wechat::wechat.voice'}"><i class="fa fa-music"> 语音</i></a>
+										<li class="nav-item" data-type="voice">
+											<a class="nav-link {if $subscribe.reply_type eq 'voice'}active{/if}" data-toggle="tab" title="{lang key='wechat::wechat.voice'}"><i class="fa fa-music"> 语音</i></a>
 										</li>
-										<li class="nav-item video-material">
-											<a class="nav-link" data-toggle="tab" title="{lang key='wechat::wechat.video'}"><i class="fa fa-video-camera"> 视频</i></a>
+										<li class="nav-item" data-type="video">
+											<a class="nav-link {if $subscribe.reply_type eq 'video'}active{/if}" data-toggle="tab" title="{lang key='wechat::wechat.video'}"><i class="fa fa-video-camera"> 视频</i></a>
+										</li>
+										<li class="nav-item" data-type="news">
+											<a class="nav-link {if $subscribe.reply_type eq 'news'}active{/if}" data-toggle="tab" title="{lang key='wechat::wechat.text_message'}"><i class="fa fa-list-alt"> 图文</i></a>
 										</li>
 									</ul>
-							   		<div class="text m_b10 {if $subscribe.media_id}hidden{/if}">
-							   			<textarea class="m_t10 span12 form-control" name="content" cols="40" rows="5">{if $subscribe.content}{$subscribe.content}{/if}</textarea>
-							    	</div>
-							    	
-									<div class="material_picture {if empty($subscribe.media_id)}hidden{/if}">
-							        	{if $subscribe['media']}
-							       			{if $subscribe['media']['type'] == 'voice'}
-							           			<input type='hidden' name='media_id' value="{$subscribe['media_id']}"><img src="{$subscribe['media']['file']}" class='img-rounded material_show' />
-							          			<div class="material_filename">{$subscribe['media']['file_name']}</div>
-							          		{elseif $subscribe['media']['type'] == 'video'}
-							             		<input type='hidden' name='media_id' value="{$subscribe['media_id']}"><img src="{$subscribe['media']['file']}" class='img-rounded material_show' />
-							         			<div class="material_filename">{$subscribe['media']['file_name']}</div>
-							         		{else}
-							        			<input type='hidden' name='media_id' value="{$subscribe['media_id']}"><img src="{$subscribe['media']['file']}" class='img-rounded material_show' />
-							     			{/if}
-							    		{/if}
-									</div>
+			                   		<div class="text m_b10">
+			                   			<textarea class="m_t10 span12 form-control {if $subscribe.reply_type neq 'text'}hide{/if}" name="content" cols="40" rows="5" id="chat_editor">{$subscribe.content}</textarea>
+										<div class="js_appmsgArea" {if $subscribe.reply_type neq 'text'}style="display: block;"{/if}>
+											<div class="tab_cont_cover create-type__list" {if $subscribe.id}style="display: none;"{/if}>
+												<div class="create-type__item">
+													<a href="javascript:;" class="create-type__link choose_material" data-type="{$subscribe.reply_type}" data-url="{RC_Uri::url('wechat/platform_material/choose_material')}&material=1">
+														<i class="create-type__icon file"></i>
+														<strong class="create-type__title">从素材库选择</strong>
+													</a>
+												</div>
+											</div>
+											
+											{if $subscribe.reply_type neq 'text'}
+												{if $subscribe.reply_type neq 'news'}
+												<div class="img_preview">
+													<img class="preview_img margin_10" src="{$subscribe.media.file}" alt="">
+													<input type="hidden" name="media_id" value="3">
+													<a href="javascript:;" class="jsmsgSenderDelBt link_dele" "="">删除</a>
+												</div>
+												{else}
+												<div class="weui-desktop-media__list-col margin_10">
+													<li class="thumbnail move-mod-group big grid-item">
+													    <div class="article">
+													        <div class="cover">
+													            <a target="__blank" href="javascript:;">
+													                <img src="{$subscribe.media.file}" />
+													            </a>
+													            <span>{$subscribe.media.title}</span>
+													        </div>
+													    </div>
+													    <div class="edit_mask appmsg_mask">
+													        <i class="icon_card_selected">已选择</i>
+													    </div>
+													    {if $subscribe.child}
+													    <!-- {foreach from=$subscribe.child key=key item=val} -->
+													    <div class="article_list">
+													        <div class="f_l">{if $val.title}{$val.title}{else}{lang key='wechat::wechat.no_title'}{/if}</div>
+													        <a target="__blank" href="javascript:;">
+													            <img src="{$val.file}" class="pull-right" />
+													        </a>
+													    </div>
+													    <!-- {/foreach} -->
+													    {/if}
+													</li>
+													<input type="hidden" name="media_id" value="{$subscribe.media_id}" />
+												</div>
+												<a href="javascript:;" class="jsmsgSenderDelBt link_dele p_l0">删除</a>
+												{/if}
+											{/if}
+										</div>
+			                    	</div>
 								</div>
 							</div>
 						</div>
@@ -95,33 +133,6 @@
     </div>
 </div>
 
-<div class="modal fade text-left" id="add_material">
-	<div class="modal-dialog modal-lg" role="document">
-		<div class="modal-content">
-			<div class="modal-header">
-				<h3 class="modal-title">{lang key='wechat::wechat.select_material'}</h3>
-				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-				  <span aria-hidden="true">×</span>
-				</button>
-			</div>
-			
-			<div class="form material_choose" data-url="{url path='wechat/platform_response/get_material_info'}">
-				<div class="material_choose_list">
-                    <div class="material_select m_0">
-                        <table class="table smpl_tbl dataTable m_b0">
-                            <thead>
-                            </thead>
-                            <tbody class="material_select_tbody">
-                            </tbody>
-                        </table>
-					</div>
-				</div>
-		   	
-			   	<div class="modal-footer justify-content-center">
-					<input type="button" class="btn btn-outline-primary material_verify" value="{lang key='wechat::wechat.ok'}" />
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
+<!-- {include file="./library/wechat_choose_material.lbi.php"} -->
+
 <!-- {/block} -->
