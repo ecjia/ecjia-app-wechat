ad17841184547300572be9f76dabd30308faac74
王正东 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-07-26 16:19:07 +0800

    添加客服表的数据模型

--- classes/Models/WechatCustomerModel.php
+++ classes/Models/WechatCustomerModel.php
@@ -7,18 +7,26 @@ use Royalcms\Component\Database\Eloquent\Model;
 /**
  * Class WechatCustomMessage
  */
-class WechatCustomMessageModel extends Model
+class WechatCustomerModel extends Model
 {
-    protected $table = 'wechat_custom_message';
+    protected $table = 'wechat_customer';
 
     public $timestamps = false;
 
     protected $fillable = [
         'wechat_id',
-        'uid',
-        'msg',
-        'send_time',
-        'is_wechat_admin'
+        'kf_id',
+        'kf_account',
+        'kf_nick',
+        'kf_headimgurl',
+        'status',
+        'online_status',
+        'accepted_case',
+        'kf_wx',
+        'invite_wx',
+        'invite_expire_time',
+        'invite_status',
+        'file_url',
     ];
 
     protected $guarded = [];
