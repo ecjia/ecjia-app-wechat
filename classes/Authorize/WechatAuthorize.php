cae74485c1cb1d6456dfcefff5087d1aa4c752c5
王正东 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-08-07 13:00:08 +0800

    微信授权url地址封装

--- classes/Authorize/WechatAuthorize.php
+++ classes/Authorize/WechatAuthorize.php
@@ -8,17 +8,23 @@
 
 namespace Ecjia\App\Wechat\Authorize;
 
+use Ecjia\App\Wechat\WechatUUID;
+use ecjia_front;
 
 class WechatAuthorize
 {
 
-    const SNSAPI_USERINFO = 'snsapi_userinfo';
+    const SNSAPI_USERINFO   = 'snsapi_userinfo';
+    const SNSAPI_BASE       = 'snsapi_base';
 
 
     protected $scope;
 
-    public function __construct($scope = self::SNSAPI_USERINFO)
+    protected $uuid;
+
+    public function __construct($uuid, $scope = self::SNSAPI_USERINFO)
     {
+        $this->wechat = with(new WechatUUID($uuid))->getWechatInstance();
         $this->scope = $scope;
     }
 
@@ -32,21 +38,25 @@ class WechatAuthorize
             $callback_url = '';
         }
 
-        $state = md5(uniqid(rand(), true));
+        $callback_url = urlencode($callback_url);
 
-        $params = array(
-            'redirect_uri'  => $callback_url,
-            'scope'         => self::SNSAPI_USERINFO,
-            'state'         => $state,
-        );
+        $state = md5(uniqid(rand(), true));
 
         $_SESSION['wechat_authorize_state'] = $state;
 
-        $code_url = $this->oauth->getQRConnectCodeUrl($params);
+
+        $params = [
+            'appid'         => $this->wechat->config->get('app_id'),
+            'redirect_uri'  => $callback_url,
+            'response_type' => 'code',
+            'scope'         => $this->scope,
+            'state'         => $state,
+        ];
+
+        static $url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=%s&redirect_uri=%s&response_type=%s&scope=%s&state=%s#wechat_redirect';
+        $code_url = vsprintf($url, $params);
 
         return $code_url;
-
     }
-
 
 }
