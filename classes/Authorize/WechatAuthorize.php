f1cde4909a05633eabf0e115b2e177336699f07d
王正东 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-08-07 10:37:45 +0800

    更新wechat-oauth组件

--- classes/Authorize/WechatAuthorize.php
+++ classes/Authorize/WechatAuthorize.php
@@ -12,4 +12,41 @@ namespace Ecjia\App\Wechat\Authorize;
 class WechatAuthorize
 {
 
+    const SNSAPI_USERINFO = 'snsapi_userinfo';
+
+
+    protected $scope;
+
+    public function __construct($scope = self::SNSAPI_USERINFO)
+    {
+        $this->scope = $scope;
+    }
+
+
+    /**
+     * 生成授权网址
+     */
+    public function getAuthorizeUrl($callback_url = null)
+    {
+        if (is_null($callback_url)) {
+            $callback_url = '';
+        }
+
+        $state = md5(uniqid(rand(), true));
+
+        $params = array(
+            'redirect_uri'  => $callback_url,
+            'scope'         => self::SNSAPI_USERINFO,
+            'state'         => $state,
+        );
+
+        $_SESSION['wechat_authorize_state'] = $state;
+
+        $code_url = $this->oauth->getQRConnectCodeUrl($params);
+
+        return $code_url;
+
+    }
+
+
 }
