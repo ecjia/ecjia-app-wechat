8901250cc3675a17092c101e25e9f8c8b3136047
王正东 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comdhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2019-02-21 10:19:22 +0800

    更新应用配置文件中的语言包调用

--- configs/package.php
+++ configs/package.php
@@ -52,11 +52,11 @@ defined('IN_ECJIA') or exit('No permission resources.');
 return array(
     'identifier'  => 'ecjia.wechat',
     'directory'   => 'wechat',
-    'name'        => 'wechat',
-    'description' => 'wechat_desc',                 /* 描述对应的语言项 */
+    'name'        => __('微信公众平台'),
+    'description' => __('微信公众平台'),
     'author'      => 'ECJIA TEAM',                  /* 作者 */
     'website'     => 'http://www.ecjia.com',        /* 网址 */
-    'version'     => '1.25.0',                      /* 版本号 */
+    'version'     => '1.27.4',                      /* 版本号 */
     'copyright'   => 'ECJIA Copyright 2014 ~ 2019.',
     'namespace'   => 'Ecjia\App\Wechat',
     'provider'    => 'WechatServiceProvider',
